7c6973f44880e02c5698379d377f2494aa0acbce
eric3 <erick.2henriquehtp@gmail.com>
2024-02-12 11:09:55 -0300

    fucionando

--- index.js
+++ index.js
@@ -7,30 +7,38 @@ let form = document.querySelector('form')
 // let errorname = document.getElementById('errorname')
 
 
-form.addEventListener('submit', (e) =>{
-    if(name_.value === ""){
+form.addEventListener('submit', (e) => {
+    if (name_.value === "") {
         errorname.innerText = "Por favor digite seu nome"
     }
-    if(password.value === ""){
+    if (password.value === "") {
         errorpassword.innerText = "Por favor digite sua senha"
     }
-    if(email.value === ""){
+    if (email.value === "") {
         erroremail.innerText = "Por favor digite seu email"
     }
-    if(telefone.value === ""){
+    if (telefone.value === "") {
         errortelefone.innerText = "Por favor digite seu telefone"
     }
 
 
-    if(email.value === true && telefone.value === true && password.value === true&&
-    name_.value === true){
+    if (email.value == true && telefone.value == true && password.value == true &&
+        name_.value == true) {
         sucessform.innerText = "Formulario enviado"
     }
 
 
-    console.log(email.value);
-    console.log(password.value);
-    console.log(name_.value);
-    console.log(telefone.value);
     e.preventDefault()
 })
+
+function validarForm() {
+    if (email.value !== "" && name_.value !== "" && telefone.value !== "" && password.value !== "") {
+        sucessform.innerText = "Formulario enviado com sucesso!"
+
+
+        console.log(email.value);
+        console.log(password.value);
+        console.log(name_.value);
+        console.log(telefone.value);
+    }
+}
