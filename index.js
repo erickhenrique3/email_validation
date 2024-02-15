41e51b6242b1073f331c2fd55d548838950983ee
eric3 <erick.2henriquehtp@gmail.com>
2024-02-15 19:45:21 -0300

    pronto

--- index.js
+++ index.js
@@ -5,41 +5,54 @@ let telefone = document.getElementById('number');
 let form = document.querySelector('form');
 // let errorname = document.getElementById('errorname')
 
+let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+let namePattern = /^[a-zA-Z\s]+$/;
 
 form.addEventListener('submit', (e) => {
     if (name_.value === "") {
         errorname.innerText = "Por favor digite seu nome"
+    } else if (!namePattern.test(name_.value)) {
+        errorname.innerText = "Nome inválido";
+    }
+    else {
+        errorname.innerText = ""
     }
     if (password.value === "") {
         errorpassword.innerText = "Por favor digite sua senha"
+    } else {
+        errorpassword.innerText = ""
     }
     if (email.value === "") {
         erroremail.innerText = "Por favor digite seu email"
+    } else if (!emailPattern.test(email.value)) {
+        erroremail.innerText = "Email inválido"
+    }
+    else {
+        erroremail.innerText = ""
     }
     if (telefone.value === "") {
         errortelefone.innerText = "Por favor digite seu telefone"
+    } else {
+        errortelefone.innerText = ""
     }
 
 
-    if (email.value == true && telefone.value == true && password.value == true &&
-        name_.value == true) {
-        sucessform.innerText = "Formulario enviado"
-    }
+
 
 
     e.preventDefault()
 })
 
 function validarForm() {
-    if (email.value !== "" && name_.value !== "" && telefone.value !== "" && password.value !== "") {
+    if (emailPattern.test(email.value) && namePattern.test(name_.value) && telefone.value !== "" && password.value !== "") {
         sucessform.innerText = "Formulario enviado com sucesso!";
-        
-        // removendo os p//
-        let paragrafos = document.querySelectorAll('p');
 
-        paragrafos.forEach(function(paragrafos){
-                     paragrafos.remove();
-        });
+        // removendo os p//
+        // let paragrafos = document.querySelectorAll('p');
+
+        // paragrafos.forEach(function(paragrafos){
+        //              paragrafos.remove();
+        // });
 
 
         console.log(email.value);
@@ -47,4 +60,7 @@ function validarForm() {
         console.log(name_.value);
         console.log(telefone.value);
     }
+
+
 }
+
